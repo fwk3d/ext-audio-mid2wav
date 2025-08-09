7e3834b88689eed9ce83327f5b222b46384dbb95
r-lyeh <35402248+r-lyeh@users.noreply.github.com>
2025-08-09 14:45:47 +0200

    migrate to v3

--- api.h
+++ api.h
@@ -2,10 +2,10 @@
 
 AUTORUN {
     recipe(
-        // paths and extensions. expected errorlevel
+        // paths and extensions. final ziplevel
         "**.mid", 0,
         // conversion steps
-        "ext/ext-audio-mid2wav/mid2wav.EXE INPUT OUTPUT ext/ext-audio-mid2wav/FluidR3_GM.sf3\n"
+        "ext/ext-audio-mid2wav/mid2wav.EXE INPUT OUTPUT.wav ext/ext-audio-mid2wav/FluidR3_GM.sf3\n"
     );
 }
 
